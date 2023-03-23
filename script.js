c3a1be81ecd0be2962f4133fd5d52e4d60cba390
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-03-23 17:50:27 -0400

    updated variables list

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 
-var specChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var numBers = "0123456789";
-var uAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lAlpha = "abcdefghijklmnopqrstuvwxyz";
+var specChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var numBers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var uAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 
