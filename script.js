a9424e3c8a6a346991afd3355ad7142d7d33bd81
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-03-23 21:26:47 -0400

    used concat to combine values

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* calls the generate button that will start the pw generator */
 var generateBtn = document.querySelector("#generate");
 /* the variables that contain the characters that make up our generated password */
-var specChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-var numBers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var uAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specChar = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",","-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+var numBers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var uAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var paSelect = [];
-/* Function that customizes the random password */
-function customizePassword () {
-/* prompt and confirm meassges that interact with the user to help generate a custom password */
+/* Function that generates the random password based on user's selections */
+function generatePassword () {
+/* interacts with the user to help generate a custom password */
 var pwLegnth = prompt("How long would you like your password to be? NOTE: Please choose a numeric value of at least 8 and no more than 128");
 var pwlAlpha = confirm("Would you like your password to contain lowercase letters?");
 var pwuAlpha = confirm("Would you like your password to contain uppercase letters?");
@@ -19,7 +19,7 @@ if (pwLegnth < 8 || pwLegnth > 128) {
 alert("Please choose a numeric value between 8 and 128 and try again!");
 } 
 else {
-  alert("Your password will contain" + pwLegnth + "characters.");
+  alert("Your password will contain " + pwLegnth + " characters.");
 }
 if (pwlAlpha) {
   alert("Your password will contain lowercase letters.");
@@ -43,8 +43,22 @@ if (pwspecChar) {
   alert("Your password will contain special characters.");
 }
 else {
-  alert("Your password will not contain special characters.")
+  alert("Your password will not contain special characters.");
 }
+/* combines user selections to generate values */
+if (pwlAlpha) {
+  paSelect = paSelect.concat(pwlAlpha);
+}
+if (pwuAlpha) {
+  paSelect = paSelect.concat(pwuAlpha);
+}
+if (pwnumBers) {
+  paSelect = paSelect.concat(pwnumBers);
+}
+if (pwspecChar) {
+  paSelect = paSelect.concat(pwspecChar);
+}
+
 }
 
 function writePassword() {
